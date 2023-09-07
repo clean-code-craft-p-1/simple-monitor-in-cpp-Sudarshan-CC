ea60700cd286223858b5f2a15e0a17ab7a8a638b
Sudarshan-CC <139843052+Sudarshan-CC@users.noreply.github.com>
2023-09-07 17:43:43 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -25,44 +25,62 @@ bool isTemperatureWarning(float temperature) {
     return temperature >= 102 - warningTolerance || temperature <= 95 + warningTolerance;
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(temperature > 102 || temperature < 95) {
-    cout << "Temperature critical!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
+bool checkTemperature(float temperature) {
 
-    return 0;
-  } else if(pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
+    if (isTemperatureWarning(temperature)) {
+        printWarningMessage();
     }
-    return 0;
-  } else if(spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
+    else if (istemperatureCritical(temperature)) {
+        printCriticalMessage("Temperature critical!");
+        return false;
     }
-    return 0;
-  }
-  return 1;
+    return true;
+}
+
+
+bool ispulseRateCritical(float pulseRate){
+    return pulseRate < 60 || pulseRate > 100;
+}
+bool ispulseRateWarning(float pulseRate)
+{   
+    float warningTolerance = 100 * 0.015;
+    return pulseRate >= 100 - warningTolerance || pulseRate <= 60 + warningTolerance;
+}
+
+bool checkPulseRate(float pulseRate) {
+    
+    if (ispulseRateWarning(pulseRate)) {
+        printWarningMessage();
+    }
+    else if (ispulseRateCritical(pulseRate)) {
+        printCriticalMessage("Pulse Rate is out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool checkSpo2(float spo2) {
+    float warningTolerance = 90 * 0.015;
+    if (spo2 < 90) {
+        printCriticalMessage("Oxygen Saturation out of range!");
+        return false;
+    }
+    else if (spo2 <= 90 - warningTolerance) {
+        printWarningMessage();
+    }
+    return true;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+return checkTemperature(temperature) && checkPulseRate(pulseRate) && checkSpo2(spo2);
 }
 
 int main() {
   assert(!vitalsOk(99, 102, 70));
   assert(vitalsOk(98.1, 70, 98));
+  assert(!vitalsOk(103,72,95));
+  assert(!vitalsOk(94,72,95));
+  assert(!vitalsOk(98.5,72,60);
+    
   cout << "Done\n";
 }
